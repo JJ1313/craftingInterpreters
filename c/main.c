11a99a6441ef77289baa77e7749f8415ab0e2d6c
JJ <yeiyeithirteen@gmail.com>
2024-08-18 19:53:27 -0400

    Agrega funcionalidad Delete, Insert, Display and Find a la Doubly Linked List

--- c/main.c
+++ c/main.c
@@ -6,7 +6,8 @@
 
 #include <stdio.h>
 #include <string.h>
- 
+#include <stdlib.h>
+
 // ======= DOUBLY LINK LIST ======
 // Node
 struct DoublyLinkedNode {
@@ -23,8 +24,12 @@ struct DoublyLinkedList {
 typedef struct DoublyLinkedList doublyLinkedList;
 
 // Mostrar 
-void showList(doublyLinkedList list){
-  node_dl* temp = list.head;
+void showList(doublyLinkedList* list){
+  node_dl* temp = list->head;
+  if(temp == NULL){
+    printf("Empty list\n");
+    return;
+  }
   while(temp){
     if(temp->next){
       printf("%s ", temp->word);
@@ -38,29 +43,76 @@ void showList(doublyLinkedList list){
 }
 
 // Insert
-int insertDoublyLinkedList(doublyLinkedList* list, char* word, int pos){
+int insert(doublyLinkedList* list, char* word, int pos){
+  node_dl* temp = list->head;
+  int i = 0;
+  node_dl* node = (node_dl*) malloc(sizeof(node_dl));
+  if (node == NULL){
+    printf("Error: Memory allocation failure");
+    return -1;
+  }
+  strcpy(node->word, word);
+  // Handle empty list
+  node->prev = NULL;
+  node->next = NULL;
+  if(temp == NULL){
+    list->tail = node;
+    list->head = node;
+    return 0;
+  }
+  while(temp){
+    if(i == pos){
+      // Inserting at the start of the list
+      if(temp->prev == NULL){
+        list->head = node;
+
+        node->next = temp;
+
+        temp->prev = node;
+      }
+      // Inserting inside the list
+      else{
+        node->prev = temp->prev;
+        node->next = temp;
+
+        temp->prev->next = node;
+        temp->prev = node;
+      }
+      return 0;
+    }
+    i += 1 ;
+    temp = temp->next;
+  }
+  // Inserting at the end of the list, doesn't matter if it's out of bound
+  if(temp == NULL){
+    node->prev = list->tail;
+
+    list->tail->next = node;
+    list->tail = node;
+  }
   return 0;
 }
 
 // Find
-int findDoublyLinkedList(doublyLinkedList* list, char* word){
+int find(doublyLinkedList* list, char* word){
   int i = 0;
   node_dl* temp = list->head;
 
+  printf("Searching...\n");
   while(temp){
     if(strcmp(word, temp->word) == 0){
-      printf("Searching...\n");
       printf("'%s' is at position %d\n", word, i);
       return i;
     }
     i += 1;
     temp = temp->next;
   }
+  printf("'%s' is not in the list\n", word);
   return -1;
 }
 
 // Delete
-int deleteDoublyLinkedList(doublyLinkedList* list, char* word){
+int delete(doublyLinkedList* list, char* word){
   node_dl* temp = list->head;
   while(temp){
     if(strcmp(word, temp->word) == 0){
@@ -89,28 +141,14 @@ int deleteDoublyLinkedList(doublyLinkedList* list, char* word){
 // ====== MAIN ======
 int main(){
   doublyLinkedList l1;
-  node_dl n1, n2, n3;
   
-  strcpy(n1.word, "Many");
-  strcpy(n2.word, "is");
-  strcpy(n3.word, "fat");
- 
-  n1.next = &n2;
-  n2.next = &n3;
-  n3.next = NULL;
-  
-  n1.prev = NULL;
-  n2.prev = &n1;
-  n3.prev = &n2;
-
-  l1.head = &n1;
-  l1.tail = &n3;
-
-  showList(l1);
-  findDoublyLinkedList(&l1, "is");
-  deleteDoublyLinkedList(&l1, "is");
-  showList(l1);
-  insertDoublyLinkedList(&l1, "es", 1);
-  showList(l1);
+  showList(&l1);
+  // find(&l1, "is");
+  // delete(&l1, "is");
+  // showList(&l1);
+  insert(&l1, "Many", -1);
+  insert(&l1, "is", -1);
+  insert(&l1, "fat", -1);
+  showList(&l1);
   return 0;
 }
